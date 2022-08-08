57381040f03b071b511af66d5e564958cd8e0949
karkar <yiam.kar.kar@huawei.com>
2022-08-08 14:51:39 +0800

    update html

--- index.js
+++ index.js
@@ -11,12 +11,31 @@ const appVersion = "4.0";
 
 app.get('/', (req, res) => {
 
-  var msg = `<h1>Hello World!</h1>
-   <h2>
-    Process ID: ${pid} <br> 
-    Running on: ${hostname} <br>
-    App Version: ${appVersion}
-   </h2>`
+  var msg = `<head> <style>
+    #grad {
+    height: 1000px;
+    width: 100%;
+    background-color: green;/* For browsers that do not support gradients */
+    background-image: linear-gradient(to bottom right, #33ccff 0%, #ccffcc 100%);
+    }
+    html, body {
+    height: 100%;
+    }
+    html {
+    display: table;
+    margin: auto;
+    }
+    body {
+    display: table-cell;
+    vertical-align: left;
+    }
+    </style></head>
+    <h1>Hello World!</h1>
+    <h2>
+        Process ID: ${pid} <br> 
+        Running on: ${hostname} <br>
+        App Version: ${appVersion}
+    </h2>`
 
   res.send(msg)
 })
