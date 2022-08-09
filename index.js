70618ceb2bfe28813590dc2facfe6940b0cca8b2
karkar <yiam.kar.kar@huawei.com>
2022-08-09 14:55:15 +0800

    init all files

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-'use strict';
+const express = require('express')
+const app = express()
+const port = 3000
 
-const express = require('express');
-
-// Constants
-const PORT = 8080;
 var os = require('os')
-var HOST = os.hostname();
+var hostname = os.hostname();
+
 var pid = process.pid;
+
 const appVersion = "4.0";
 
-// App
-const app = express();
 app.get('/', (req, res) => {
 
   var msg = `<html><head><title>Simple NodeJS App</title></head>
-  <body style="background: linear-gradient(to bottom, #ccffcc 0%, #ffffff 100%);">
+  <body style="background: linear-gradient(to top right, #33ccff 0%, #ff9966 100%)">
   <h1><p style="color:black;">Hello World!</p></h1>
   <h2><p style="color:maroon;">
       ----------------------<br>
       INFORMATION <br>
       ---------------------- <br>
       Process ID: ${pid} <br> 
-      Running on: ${HOST} <br>
+      Running on: ${hostname} <br>
       App Version: ${appVersion}
   </p></h2>
   </body></html>`
-  res.send(msg);
-});
 
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+  res.send(msg)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
